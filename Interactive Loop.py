4e03c0552540b24d4cbd5773afaa7e4afc48aa8f
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-20 23:25:02 -0700

    started the program

---
+++ Interactive Loop.py
@@ -0,0 +1,17 @@
+# Name: Ruben Sanduleac
+# Date: October 20th 2021
+# Discription: Write a program that sums numbers obtained from the user.
+#              Numbers in the range of -100 to 100 are valid. 
+#              A number outside this range indicates that the user is finished with their input.
+#              Print the sum of the numbers when complete.
+
+def main():
+
+    # print the title of the program
+    print("\nInteractive Loop\n")
+
+    total  = 0
+
+    
+
+main()
