75858631cda9f7d37fd266767bb52017cf2c78f7
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-22 00:32:18 -0700

    finished

--- Interactive Loop.py
+++ Interactive Loop.py
@@ -9,9 +9,20 @@ def main():
 
     # print the title of the program
     print("\nInteractive Loop\n")
+    print("Enter a number between -100 and 100 and I will add the integer to the total\n")
+    print("Enter any integer outside the range to quit.\n\n")
 
+    #holds the sum of the integers entered
     total  = 0
 
-    
+    while True:
+        number = int(input("Enter a number: "))
 
+        if(number < -101) or (number > 101):
+            break
+
+        total += number
+
+    print("Total: ", total)
+    
 main()
