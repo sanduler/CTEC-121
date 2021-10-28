882d0c384a07a9367d852f783a00630054ace283
Sanduler <50250694+sanduler@users.noreply.github.com>
2021-10-27 22:36:00 -0700

    finished

--- module-6---problem-set-5-emilinac-master/problem-set-6-problem-1.py
+++ module-6---problem-set-5-emilinac-master/problem-set-6-problem-1.py
@@ -36,16 +36,19 @@ def snowman():
     center = graphics.Point(400,250)
     circle1 = graphics.Circle(center,60)
     circle1.setFill("White")
+    circle1.setWidth(4)
     circle1.draw(win)
 
     center2 = graphics.Point(400,400)
     circle2 = graphics.Circle(center2,90)
     circle2.setFill("White")
+    circle2.setWidth(4)
     circle2.draw(win)
 
     center3 = graphics.Point(400,610)
     circle3 = graphics.Circle(center3,120)
     circle3.setFill("White")
+    circle3.setWidth(4)
     circle3.draw(win)
 
 
@@ -65,6 +68,13 @@ def snowman():
     # draw a nose using the polygon method and make it orange
     # name the nose using the variable nose
 
+    p1 = graphics.Point(400,250)
+    p2 = graphics.Point(400,270)
+    p3 = graphics.Point(500,250)
+    nose = graphics.Polygon(p1,p2,p3)
+    nose.setFill("Orange")
+    nose.draw(win)
+
     # draw a hat using a rectangle and fill it with black
     # name the hat using the variable hat
 
@@ -75,12 +85,23 @@ def snowman():
     # draw a line to represent the rim of the hat using a line
     # name the line using the variable hatline
 
-    hatline = graphics.Line(graphics.Point(100,100),graphics.Point(200,200))
+    hatline = graphics.Line(graphics.Point(300,200),graphics.Point(500,200))
     hatline.setFill("Black")
+    hatline.setWidth(4)
     hatline.draw(win)
 
     # draw two arms that connect to the body of the snowman
     # name the line objects leftArm and rightArm
+
+    leftArm = graphics.Line(graphics.Point(200,420),graphics.Point(325,350))
+    leftArm.setFill("Black")
+    leftArm.setWidth(4)
+    leftArm.draw(win)
+
+    rightArm = graphics.Line(graphics.Point(485,370),graphics.Point(610,300))
+    rightArm.setFill("Black")
+    rightArm.setWidth(4)
+    rightArm.draw(win)
 
     # close the program
     input('Press enter to quit the program ')
